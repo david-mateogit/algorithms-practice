85f07e4c68625dae8dc0999ca31eb01aeaa21270
david-mateogit <davidmateo@outlook.com>
2020-04-05 19:44:52 -0400

    addDigits

---
+++ addTwoDigits.ts
@@ -0,0 +1,18 @@
+function addTwoDigits(n: any): number {
+  const str = String(n);
+  return str
+    .split("")
+    .reduce((acc: number, curr: string) => (acc += parseInt(curr)), 0);
+}
+
+console.log(addTwoDigits(29)); // 11
+console.log(addTwoDigits(2934234)); // 27
+
+/*
+you are given a two-digit integer n. Return the sum of its digits.
+
+**Example**
+
+For n = 29, the output should be
+addTwoDigits(n) = 11.
+*/
