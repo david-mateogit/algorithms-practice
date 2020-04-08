ec5ce7af76e76aa05192d4a3b2a97b02f01e8502
david-mateogit <davidmateo@outlook.com>
2020-04-08 11:50:58 -0400

    Are Equally Strong

---
+++ areEquallyStrong.ts
@@ -0,0 +1,34 @@
+function areEquallyStrong(...args): boolean {
+  function compare(a, b): number {
+    return a - b;
+  }
+
+  const person1 = args.slice(0, 2).sort(compare);
+  const person2 = args.slice(2, 4).sort(compare);
+
+  if (person1[0] === person2[0] && person1[1] === person2[1]) {
+    return true;
+  }
+  return false;
+}
+
+console.log(areEquallyStrong(10, 15, 15, 10));
+console.log(areEquallyStrong(15, 10, 15, 10));
+console.log(areEquallyStrong(15, 10, 15, 9));
+
+/*
+Call two arms equally strong if the heaviest weights they each are able to lift are equal.
+
+Call two people equally strong if their strongest arms are equally strong (the strongest arm can be both the right and the left), and so are their weakest arms.
+
+Given your and your friend's arms' lifting capabilities find out if you two are equally strong.
+
+**Example**
+
+- For yourLeft = 10, yourRight = 15, friendsLeft = 15 and friendsRight = 10, the output should be
+areEquallyStrong(yourLeft, yourRight, friendsLeft, friendsRight) = true;
+- For yourLeft = 15, yourRight = 10, friendsLeft = 15 and friendsRight = 10, the output should be
+areEquallyStrong(yourLeft, yourRight, friendsLeft, friendsRight) = true;
+- For yourLeft = 15, yourRight = 10, friendsLeft = 15 and friendsRight = 9, the output should be
+areEquallyStrong(yourLeft, yourRight, friendsLeft, friendsRight) = false.
+*/
