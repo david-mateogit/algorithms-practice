e849ead451f070b89699ffd1c32106cae003ddb9
david-mateogit <davidmateo@outlook.com>
2020-04-08 11:19:00 -0400

    Alternating Sums

---
+++ alternatingSums.ts
@@ -0,0 +1,30 @@
+function alternatingSums(a: number[]): number[] {
+  const team1: number[] = [];
+  const team2: number[] = [];
+  a.forEach((n, index) => {
+    if (index % 2 === 0) {
+      team1.push(n);
+    } else {
+      team2.push(n);
+    }
+  });
+  const result = [
+    team1.reduce((acc, curr) => acc + curr, 0),
+    team2.reduce((acc, curr) => acc + curr, 0),
+  ];
+
+  return result;
+}
+
+console.log(alternatingSums([50, 60, 60, 45, 70]));
+
+/*
+Several people are standing in a row and need to be divided into two teams. The first person goes into team 1, the second goes into team 2, the third goes into team 1 again, the fourth into team 2, and so on.
+
+You are given an array of positive integers - the weights of the people. Return an array of two integers, where the first element is the total weight of team 1, and the second element is the total weight of team 2 after the division is complete.
+
+**Example**
+
+For a = [50, 60, 60, 45, 70], the output should be
+alternatingSums(a) = [180, 105].
+*/
